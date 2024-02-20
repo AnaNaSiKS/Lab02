a957fd6907bd14a5f8c9bfabb66a731382450d03
Влад Кантамиров <kantamirov2016@gmail.com>
2024-02-21 00:46:54 +0300

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    String name;
+
+    double balance;
+
+    User user;
+
+    String bankName;
+}
---
+++ src/User.java
@@ -0,0 +1,18 @@
+public class User {
+    String firstName;
+
+    String name;
+
+    int id;
+
+    byte[] pin = new byte[4];
+
+    Account account;
+
+    public String getFirstName(){return firstName;}
+    public void setFirstName(String value){firstName = value;}
+
+    
+
+
+}
